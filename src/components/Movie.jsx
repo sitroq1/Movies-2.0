e83b50737cf0b45990bb7c21666a6c66618974e1
sitroq1 <sitroq@gmail.com>
2023-06-16 17:11:56 +0300

    refactor: change movie card

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,22 +4,23 @@ export default function Movie({Title: title, Plot: plot, Poster: poster, imdbID}
 
   return (
     <>
-      <div className="row">
-        <div className="col s12 m6">
-          <div className="card">
-            <div className="card-image">
-              <img src={poster} />
-              <span className="card-title">{title}</span>
-              <a className="btn-floating halfway-fab waves-effect waves-light red">
-                <i className="material-icons">add</i>
-              </a>
-            </div>
-            <div className="card-content">
-              <p>{plot}</p>
-            </div>
-          </div>
-        </div>
-      </div>
+
+<div className="card">
+    <div className="card-image waves-effect waves-block waves-light">
+      <img className="activator" src={poster}/>
+    </div>
+    <div className="card-content">
+      <span className="card-title activator grey-text text-darken-4">{title}</span>
+      
+    </div>
+    <div className="card-reveal">
+      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+  </div>
+
+    
+
     </>
   );
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,22 +1,32 @@
-import React, {useEffect, useState} from 'react'
-import Movies from '../components/Movies'
-
+import React, { useEffect, useState } from "react";
+import Movies from "../components/Movies";
 
 export default function Main() {
 
-    const [movies, setMovies] = useState([])
+  const [error, setError] = useState(null);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        fetch('http://www.omdbapi.com/?i=tt3896198&apikey=58c515fd&s=matrix')
-            .then((res) => res.json())
-            .then((res) => setMovies(res.Search))
-    }, [])
+  useEffect(() => {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=58c515fd&s=matrix")
+      .then((res) => res.json())
+      .then(
+        (res) => {
+          setMovies(res.Search);
+        },
+        (error) => {
+          setError(error);
+        }
+      );
+  }, []);
 
-    console.log(movies)
-    
+  if (error) {
+    console.log(error.message);
+  }
+
+
   return (
     <main className="content">
-        <Movies movies={movies} />
+      <Movies movies={movies} />
     </main>
-  )
+  );
 }
